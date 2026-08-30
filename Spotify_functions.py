c86bbc3180cf4c2cef8068a2786a575cd7fe200b
LuisGBA23 <72234306+LuisGBA23@users.noreply.github.com>
2026-08-30 13:19:34 -0600

    updated readme

--- Spotify_functions.py
+++ Spotify_functions.py
@@ -118,7 +118,7 @@ if __name__ == "__main__":
     hilo= threading.Thread(target= update_cached_songs)
     hilo.start()
 
-    print("Configurando unas cosas por ahí we", end= '')
+    print("Configurando unas cosas por ahí", end= '')
     time.sleep(0.5)
     while True:
         if its_all_ready:
